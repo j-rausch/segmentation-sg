d40e3d58aebbd3d95ec183941ed62d6913ffc778
j-rausch <jrausch@inf.ethz.ch>
2022-01-30 13:05:07 +0100

    Add script to reproduce baseline predcls experiment

--- scripts/train_predcls.py
+++ scripts/train_predcls.py
@@ -11,7 +11,7 @@ from detectron2.engine import default_argument_parser, default_setup, launch
 from detectron2.config import get_cfg
 from detectron2.checkpoint import DetectionCheckpointer
 
-from segmentationsg.engine import SceneGraphSegmentationTrainer
+from segmentationsg.engine import SceneGraphTrainer
 from segmentationsg.data import add_dataset_config, VisualGenomeTrainData, register_datasets
 from segmentationsg.modeling.roi_heads.scenegraph_head import add_scenegraph_config
 from detectron2.data import DatasetCatalog, MetadataCatalog
@@ -19,16 +19,6 @@ from detectron2.data.datasets import register_coco_instances
 from segmentationsg.modeling import *
 
 parser = default_argument_parser()
-
-def register_coco_data(args):
-    # annotations = json.load(open('/h/skhandel/SceneGraph/data/coco/instances_train2014.json', 'r'))
-    # classes = [x['name'] for x in annotations['categories']]
-    classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
-    MetadataCatalog.get('coco_train_2014').set(thing_classes=classes, evaluator_type='coco')
-    annotations = args.DATASETS.MSCOCO.ANNOTATIONS
-    dataroot = args.DATASETS.MSCOCO.DATAROOT
-    register_coco_instances("coco_train_2017", {}, annotations + 'instances_train2017.json', dataroot + '/train2017/')
-    register_coco_instances("coco_val_2017", {}, annotations + 'instances_val2017.json', dataroot + '/val2017/')
 
 def setup(args):
     cfg = get_cfg()
@@ -39,7 +29,6 @@ def setup(args):
     cfg.merge_from_list(args.opts)
     cfg.freeze()
     register_datasets(cfg)
-    register_coco_data(cfg)
     default_setup(cfg, args)
     setup_logger(output=cfg.OUTPUT_DIR, distributed_rank=comm.get_rank(), name="LSDA")
     return cfg
@@ -48,16 +37,16 @@ def main(args):
     cfg = setup(args)
     if args.eval_only:
         
-        model = SceneGraphSegmentationTrainer.build_model(cfg)
+        model = SceneGraphTrainer.build_model(cfg)
         DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
             cfg.MODEL.WEIGHTS, resume=args.resume
         )
-        res = SceneGraphSegmentationTrainer.test(cfg, model)
+        res = SceneGraphTrainer.test(cfg, model)
         # if comm.is_main_process():
         #     verify_results(cfg, res)
         return res
         
-    trainer = SceneGraphSegmentationTrainer(cfg)
+    trainer = SceneGraphTrainer(cfg)
     trainer.resume_or_load(resume=args.resume)
     return trainer.train()
 
